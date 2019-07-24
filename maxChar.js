6d994bfda6e282a53a199b1541cc3025c8dd0bf7
Steve Kim <suykim21@Steves-MacBook.local>
2019-07-23 20:08:38 -0700

    adde maxChar algos

---
+++ maxChar.js
@@ -0,0 +1,19 @@
+// Find max character of the string ex: hello => l
+function maxChar(str) {
+  let charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
