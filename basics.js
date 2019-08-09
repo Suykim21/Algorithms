feaa9c05213e0dbd0d501a6b78d6c72dabb64dc6
Steve Kim <suykim21@Steves-MacBook.local>
2019-08-08 19:43:53 -0700

    added capitalization

--- basics.js
+++ basics.js
@@ -20,3 +20,26 @@ function revInt(int) {
   const revString = int.toString().split('').reverse().join('');
   return parseInt(revString) * Math.sign(int); // Inputs negative or positive sign
 }
+
+// Sentence Capitalization
+function capitalize(str) {
+  let cap = true,
+    sentence = ""
+
+  for (let i = 0; i <= str.length - 1; i++) {
+    if (cap) {
+      sentence.concat(str[i].toUpperCase());
+      cap = false;
+    } else {
+      if (str[i] !== " ") sentence.concat(str[i])
+      else {
+        sentence.concat(str[i])
+        cap = true;
+      }
+    }
+  }
+
+  console.log(sentence);
+}
+
+capitalize("")
